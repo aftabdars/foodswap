5f2e298fd06624cea9cb80acd0a753cfdf2c3c3e
06MMG <124175643+06MMG@users.noreply.github.com>
2024-01-27 00:56:16 -0800

    little changes in Foodpreview

--- src/components/FoodPreview.js
+++ src/components/FoodPreview.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { StyleSheet, TouchableOpacity, View, Image, Text } from "react-native";
+import { StyleSheet, TouchableOpacity, View, Image, Text, ImageBackground } from "react-native";
 import { ThemeContext, getColors } from '../assets/Theme';
 import { formatTimeDifferenceFuture } from "../utils/Format";
 import { navigationPreviousScreenName, stringCapitalize } from "../utils/Utils";
@@ -29,20 +29,18 @@ function FoodPreview(props) {
 
   return (
     <TouchableOpacity style={[styles.container, props.style]} onPress={handlePress}>
-      {foodData ?
-        <Image
+      {foodData ? (
+        <ImageBackground
           source={{ uri: foodData.image }}
           resizeMode="cover"
           style={styles.image}
-        />
-        :
-        <View style={styles.imageAlternateContainer} />
-      }
-
+          imageStyle={styles.imageOverlay}
+        >
       <View style={styles.overlay}>
-        <Text style={styles.title}>
+        <Text style={styles.title}numberOfLines={2}>
           {foodData &&
-            foodData.name.substring(0, TITLE_MAX_LENGTH) + (foodData.name.length > TITLE_MAX_LENGTH ? '...' : '')
+            foodData.name.substring(0, TITLE_MAX_LENGTH) + 
+            (foodData.name.length > TITLE_MAX_LENGTH ? '...' : '')
           }
         </Text>
         <Text style={styles.infoText}>
@@ -52,8 +50,16 @@ function FoodPreview(props) {
             `Expires In: ${formatedExpiredTime.difference}`
           }
         </Text>
-        <Text style={styles.infoText}>{foodData && `Up for: ${foodData.up_for && stringCapitalize(foodData.up_for)}`}</Text>
+        <View style={styles.infoContainer}>
+        <Text style={styles.infoText}>
+        {foodData && `Up for: ${foodData.up_for && stringCapitalize(foodData.up_for)}`}
+        </Text>
       </View>
+      </View>
+      </ImageBackground>
+      ) : (
+        <View style={styles.imageAlternateContainer} />
+      )}
     </TouchableOpacity>
   );
 }
@@ -62,14 +68,18 @@ function createStyles(colors) {
   return StyleSheet.create({
     container: {
       borderRadius: 16,
-      backgroundColor: colors.background2,
       overflow: 'hidden',
       marginBottom: 20,
+      elevation:5,
     },
     image: {
       width: '100%',
       height: 200,
       borderRadius: 16,
+      overflow:'hidden',
+    },
+    imageOverlay:{
+      borderRadius:16,
     },
     imageAlternateContainer: { 
       width: '100%', 
@@ -78,31 +88,33 @@ function createStyles(colors) {
       backgroundColor: colors.background2 
     },
     overlay: {
-      position: 'absolute',
-      backgroundColor: 'rgba(0, 0, 0, 0.1)',
-      justifyContent: "center",
-      alignItems: "center",
-      padding: 6,
-      left: 0,
-      right: 0,
-      bottom: 0,
+      ...StyleSheet.absoluteFillObject,
+      justifyContent: 'flex-end',
+      padding: 16,
+      borderRadius:16,
+      backgroundColor: 'rgba(0, 0, 0, 0.2)',
     },
     title: {
       fontFamily: "roboto-regular",
       color: '#FFFFFF',
-      fontSize: 20,
+      fontSize: 18,
       fontWeight: 'bold',
       textAlign: 'center',
-      borderRadius: 16,
-      marginVertical: 2
+      marginBottom:8,
+
+    },
+    infoContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
     },
     infoText: {
       fontFamily: "roboto-regular",
-      fontSize: 16,
+      fontSize: 14,
       color: '#FFFFFF',
       fontWeight: 'bold',
       textAlign: "center",
-      borderRadius: 10,
+      marginBottom:4,
+      opacity:0.8,
     },
   }
   )
